78a2d1df2167f826f5eeb86dd65ed0f0889f5a9d
0x26B <williambrumble@protonmail.com>
2023-06-20 17:38:09 -0500

    Added stream handler setup, logging, and token to window creation.

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,16 +1,30 @@
 import webview
-from logging import getLogger, NullHandler
+from logging import getLogger, NullHandler, StreamHandler, DEBUG, Formatter
 
 from factory import Factory
 
-logger = getLogger(__name__)
+logger = getLogger()
 logger.addHandler(NullHandler())
+logger.setLevel(DEBUG)
+
+ch = StreamHandler()
+ch.setLevel(DEBUG)
+formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+logger.debug("TESTING 123")
 
 if __name__ == '__main__':
+    logger.debug("The application had started")
 
+    logger.debug("Using the factory to create the app")
     application = Factory.create_app(simulate=True)
-    server = Factory.create_server(application=application)
-    window = Factory.create_window(server=server.flask_app, window_name="pylogix")
 
-    webview.token = "test"
+    logger.debug("Using the factory to create the server")
+    server = Factory.create_server(application=application)
+
+    logger.debug("Using the factory to create the window")
+    window = Factory.create_window(server=server.flask_app, server_port=5000, token="test", window_name="pylogix")
+
     webview.start(debug=True)

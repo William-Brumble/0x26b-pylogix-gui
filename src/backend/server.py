7934434d58cd2369221553cc06b532fdb1e366f1
0x26B <williambrumble@protonmail.com>
2023-06-22 07:30:24 -0500

    Updated all routes to POST so that I can use the body of the request, you can with GET, but it's not recommended.

--- src/backend/server.py
+++ src/backend/server.py
@@ -2,6 +2,7 @@ import json
 import webview
 from dataclasses import asdict
 from flask import Flask, jsonify, render_template, request, Response
+from flask_cors import CORS
 from logging import getLogger, NullHandler
 
 from app import App
@@ -36,6 +37,7 @@ class Server:
         self.flask_app = Flask(__name__, static_folder=frontend_path, template_folder=frontend_path)
         self.flask_app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1  # disable caching
         self.application = application
+        self.cors = CORS(self.flask_app, resources={r"/*": {"origins": "*"}})
 
         """ Defining routes in init for simple way to wrap flask in a class """
 
@@ -43,25 +45,25 @@ class Server:
         def __add_header(response):
             return self.add_header(response)
 
-        @self.flask_app.route("/", methods = ['GET'])
+        @self.flask_app.route("/", methods = ['POST'])
         def __landing():
             logger.debug("-" * 80)
             logger.debug("Flask landing route fired")
             return self.landing()
 
-        @self.flask_app.route("/connect", methods = ['GET'])
+        @self.flask_app.route("/connect", methods = ['POST'])
         def __connect():
             logger.debug("-" * 80)
             logger.debug("Flask connect route fired")
             return self.connect()
 
-        @self.flask_app.route("/close", methods = ['GET'])
+        @self.flask_app.route("/close", methods = ['POST'])
         def __close():
             logger.debug("-" * 80)
             logger.debug("Flask close route fired")
             return self.close()
 
-        @self.flask_app.route("/get-connection-size", methods = ['GET'])
+        @self.flask_app.route("/get-connection-size", methods = ['POST'])
         def __get_connection_size():
             logger.debug("-" * 80)
             logger.debug("Flask get connection size route fired")
@@ -73,7 +75,7 @@ class Server:
             logger.debug("Flask set connection size route fired")
             return self.set_connection_size()
 
-        @self.flask_app.route("/read", methods = ['GET'])
+        @self.flask_app.route("/read", methods = ['POST'])
         def __read():
             logger.debug("-" * 80)
             logger.debug("Flask read route fired")
@@ -85,7 +87,7 @@ class Server:
             logger.debug("Flask write route fired")
             return self.write()
 
-        @self.flask_app.route("/get-plc-time", methods = ['GET'])
+        @self.flask_app.route("/get-plc-time", methods = ['POST'])
         def __get_plc_time():
             logger.debug("-" * 80)
             logger.debug("Flask get plc time route fired")
@@ -97,37 +99,37 @@ class Server:
             logger.debug("Flask set plc time route fired")
             return self.set_plc_time()
 
-        @self.flask_app.route("/get-tag-list", methods = ['GET'])
+        @self.flask_app.route("/get-tag-list", methods = ['POST'])
         def __get_tag_list():
             logger.debug("-" * 80)
             logger.debug("Flask get tag list route fired")
             return self.get_tag_list()
 
-        @self.flask_app.route("/get-program-tag-list", methods = ['GET'])
+        @self.flask_app.route("/get-program-tag-list", methods = ['POST'])
         def __get_program_tag_list():
             logger.debug("-" * 80)
             logger.debug("Flask get program tag list route fired")
             return self.get_program_tag_list()
 
-        @self.flask_app.route("/get_programs_list", methods = ['GET'])
+        @self.flask_app.route("/get-programs-list", methods = ['POST'])
         def __get_programs_list():
             logger.debug("-" * 80)
             logger.debug("Flask get programs list route fired")
             return self.get_programs_list()
 
-        @self.flask_app.route("/discover", methods = ['GET'])
+        @self.flask_app.route("/discover", methods = ['POST'])
         def __discover():
             logger.debug("-" * 80)
             logger.debug("Flask discover route fired")
             return self.discover()
 
-        @self.flask_app.route("/get-module-properties", methods = ['GET'])
+        @self.flask_app.route("/get-module-properties", methods = ['POST'])
         def __get_module_properties():
             logger.debug("-" * 80)
             logger.debug("Flask get module properties route fired")
             return self.get_module_properties()
 
-        @self.flask_app.route("/get-device-properties", methods = ['GET'])
+        @self.flask_app.route("/get-device-properties", methods = ['POST'])
         def __get_device_properties():
             logger.debug("-" * 80)
             logger.debug("Flask get device properties route fired")

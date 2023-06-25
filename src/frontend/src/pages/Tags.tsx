fb3e4a8e9b6a9e43efdf74724c29922e25cf58b0
0x26B <williambrumble@protonmail.com>
2023-06-24 19:48:07 -0500

    Played around with storybook, added some shadcn stuff, made the html look good, next will be to clean up the code.

---
+++ src/frontend/src/pages/Tags.tsx
@@ -0,0 +1,13 @@
+const TagsContainer = () => {
+    return <TagsPresentation />;
+};
+
+const TagsPresentation = () => {
+    return (
+        <div>
+            <div>TAGS</div>
+        </div>
+    );
+};
+
+export { TagsContainer as Tags };
---
+++ src/frontend/src/stories/Tags.stories.ts
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Tags } from "@/pages/Tags.tsx";
+
+const meta = {
+    title: "pages/tags",
+    component: Tags,
+} satisfies Meta<typeof Tags>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {},
+};

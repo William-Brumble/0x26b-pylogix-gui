5adec1121e84082c98142c642bd797ce1fc46e5e
0x26B <williambrumble@protonmail.com>
2023-07-04 08:18:49 -0500

    Added temp presentation logic to see backend data on source and tags page,

--- src/frontend/src/pages/Source.tsx
+++ src/frontend/src/pages/Source.tsx
@@ -1,4 +1,21 @@
+import { discover } from "@/api";
+import { IDiscoverReq, IDiscoverRes } from "@/models/discover.ts";
+import { redirect, useLoaderData } from "react-router-dom";
+
 export function Source() {
+    const { error, error_message, data }: any = useLoaderData();
+    const encoded = data as IDiscoverRes;
+
+    if (error) {
+        alert(error_message);
+    }
+
+    const listItems = encoded.response.Value.map((device) => (
+        <li className="font-normal text-foreground">
+            {JSON.stringify(device)}
+        </li>
+    ));
+
     return (
         <div className="bg-background p-5">
             <h2 className="text-foreground mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
@@ -8,6 +25,29 @@ export function Source() {
                 These are all the found PLC sources on your local area network;
                 you can select a tag source here.
             </p>
+            <ul>{listItems}</ul>
         </div>
     );
 }
+
+export const loader = async (payload: any) => {
+    const params = new URL(payload.request.url).searchParams;
+    const token = params.get("token");
+
+    if (token) {
+        const msg: IDiscoverReq = {
+            token: token,
+        };
+
+        const sources = await discover(msg);
+
+        return {
+            error: false,
+            error_message: "no error message",
+            data: sources,
+            token: token,
+        };
+    } else {
+        return redirect("/error");
+    }
+};
--- src/frontend/src/pages/Tags.tsx
+++ src/frontend/src/pages/Tags.tsx
@@ -1,4 +1,16 @@
+import { redirect, useLoaderData } from "react-router-dom";
+
+import { IGetTagListReq, IGetTagListRes } from "@/models/get_tag_list.ts";
+import { getTagList } from "@/api";
+
 export function Tags() {
+    const { data }: any = useLoaderData();
+    const encoded = data as IGetTagListRes;
+
+    const listItems = encoded.response.Value.map((tag) => (
+        <li className="font-normal text-foreground">{JSON.stringify(tag)}</li>
+    ));
+
     return (
         <div className="bg-background p-5">
             <h2 className="text-foreground mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
@@ -8,6 +20,34 @@ export function Tags() {
                 These are all the tags found in the source PLC; you can select
                 tags for monitoring here.
             </p>
+            <ul>{listItems}</ul>
         </div>
     );
 }
+export const loader = async (payload: any) => {
+    const params = new URL(payload.request.url).searchParams;
+    const token = params.get("token");
+
+    if (token) {
+        const msg: IGetTagListReq = {
+            token: token,
+            all_tags: true,
+        };
+
+        const sources = await getTagList(msg);
+
+        if (sources.error) {
+            if (sources.status === "412 Precondition Failed") {
+                alert(sources.error_message);
+                return redirect("/source");
+            }
+        }
+
+        return {
+            data: sources,
+            token: token,
+        };
+    } else {
+        return redirect("/error");
+    }
+};

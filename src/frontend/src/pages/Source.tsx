6295a94d9c7fb5d7a4889005c9bbe1f7f399cb45
William-Brumble <williambrumble@protonmail.com>
2023-07-04 13:44:40 -0500

    Created source table

--- src/frontend/src/pages/Source.tsx
+++ src/frontend/src/pages/Source.tsx
@@ -2,6 +2,9 @@ import { discover } from "@/api";
 import { IDiscoverReq, IDiscoverRes } from "@/models/discover.ts";
 import { redirect, useLoaderData } from "react-router-dom";
 
+import { columns } from "@/components/SourcesColumns.tsx";
+import { SourceDataTable } from "@/components/SourcesDataTable.tsx";
+
 export function Source() {
     const { error, error_message, data }: any = useLoaderData();
     const encoded = data as IDiscoverRes;
@@ -9,12 +12,6 @@ export function Source() {
     if (error) {
         alert(error_message);
     }
-
-    const listItems = encoded.response.Value.map((device) => (
-        <li className="font-normal text-foreground">
-            {JSON.stringify(device)}
-        </li>
-    ));
 
     return (
         <div className="bg-background p-5">
@@ -25,7 +22,12 @@ export function Source() {
                 These are all the found PLC sources on your local area network;
                 you can select a tag source here.
             </p>
-            <ul>{listItems}</ul>
+            <div className="w-full h-full min-h-full py-10">
+                <SourceDataTable
+                    columns={columns}
+                    data={encoded.response.Value}
+                />
+            </div>
         </div>
     );
 }

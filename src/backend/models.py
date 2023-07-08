1bf410f60054f9308c6f876d681a62f51882e3be
William-Brumble <williambrumble@protonmail.com>
2023-07-08 14:32:09 -0500

    Updated models to reflect actual pyloigx responses

--- src/backend/models.py
+++ src/backend/models.py
@@ -35,7 +35,7 @@ class GetConnectionSizeReqDTO(RequestDTO):
     pass
 
 @dataclass(kw_only=True)
-class PLCConnectionSizeDTO(StatusDTO):
+class PLCConnectionSizeDTO:
     connection_size: int | None
 
 @dataclass(kw_only=True)
@@ -57,6 +57,7 @@ class ReadReqDTO(RequestDTO):
     count: int = 1
     datatype: int | None = None
 
+# The apps definition of a pylogix tag
 @dataclass(kw_only=True)
 class PLCTagDTO:
     TagName: str
@@ -67,16 +68,16 @@ class PLCTagDTO:
     Array: int = 0
     Struct: int = 1
     Size: int = 0
-    AccessRight = None
-    Internal = None
-    Meta = None
-    Scope0 = None
-    Scope1 = None
-    Bytes = None
+    AccessRight: None = None
+    Internal: None = None
+    Meta: None = None
+    Scope0: None = None
+    Scope1: None = None
+    Bytes: None = None
 
     def __repr__(self):
         props = ''
-        props += 'TagName={}, '.format(self.TagName or "")
+        props += 'TagName={}, '.format(self.TagName)
         props += 'InstanceID={}, '.format(self.InstanceID)
         props += 'SymbolType={}, '.format(self.SymbolType)
         props += 'DataTypeValue={}, '.format(self.DataTypeValue)
@@ -90,11 +91,13 @@ class PLCTagDTO:
         props += 'Scope0={} '.format(self.Scope0)
         props += 'Scope1={} '.format(self.Scope1)
         props += 'Bytes={}'.format(self.Bytes)
+
         return 'Tag({})'.format(props)
 
     def __str__(self):
+
         return '{} {} {} {} {} {} {} {} {} {} {} {} {} {}'.format(
-                self.TagName or "",
+                self.TagName,
                 self.InstanceID,
                 self.SymbolType,
                 self.DataTypeValue,
@@ -109,6 +112,76 @@ class PLCTagDTO:
                 self.Scope1,
                 self.Bytes)
 
+# Pylogix definition of a device, used for mocking
+class Tag:
+    def __init__(self,
+                 TagName: str,
+                 InstanceID: int = 10,
+                 SymbolType: int = 214,
+                 DataTypeValue: int = 1750,
+                 DataType: str = "AB:ETHERNET_MODULE:C:O",
+                 Array: int = 0,
+                 Struct: int = 1,
+                 Size: int = 0,
+                 AccessRight = None,
+                 Internal = None,
+                 Meta = None,
+                 Scope0 = None,
+                 Scope1 = None,
+                 Bytes = None):
+        self.TagName = TagName
+        self.InstanceID = InstanceID
+        self.SymbolType = SymbolType
+        self.DataTypeValue = DataTypeValue
+        self.DataType = DataType
+        self.Array = Array
+        self.Struct = Struct
+        self.Size = Size
+        self.AccessRight = AccessRight
+        self.Internal = Internal
+        self.Meta = Meta
+        self.Scope0 = Scope0
+        self.Scope1 = Scope1
+        self.Bytes = Bytes
+
+    def __repr__(self):
+        props = ''
+        props += 'TagName={}, '.format(self.TagName)
+        props += 'InstanceID={}, '.format(self.InstanceID)
+        props += 'SymbolType={}, '.format(self.SymbolType)
+        props += 'DataTypeValue={}, '.format(self.DataTypeValue)
+        props += 'DataType={}, '.format(self.DataType)
+        props += 'Array={}, '.format(self.Array)
+        props += 'Struct={}, '.format(self.Struct)
+        props += 'Size={} '.format(self.Size)
+        props += 'AccessRight={} '.format(self.AccessRight)
+        props += 'Internal={} '.format(self.Internal)
+        props += 'Meta={} '.format(self.Meta)
+        props += 'Scope0={} '.format(self.Scope0)
+        props += 'Scope1={} '.format(self.Scope1)
+        props += 'Bytes={}'.format(self.Bytes)
+
+        return 'Tag({})'.format(props)
+
+    def __str__(self):
+
+        return '{} {} {} {} {} {} {} {} {} {} {} {} {} {}'.format(
+                self.TagName,
+                self.InstanceID,
+                self.SymbolType,
+                self.DataTypeValue,
+                self.DataType,
+                self.Array,
+                self.Struct,
+                self.Size,
+                self.AccessRight,
+                self.Internal,
+                self.Meta,
+                self.Scope0,
+                self.Scope1,
+                self.Bytes)
+
+# The apps definition of a pylogix device
 @dataclass(kw_only=True)
 class PLCDeviceDTO:
     Length: int = 0
@@ -117,7 +190,7 @@ class PLCDeviceDTO:
     VendorID: int = 1
     Vendor: str = 'Rockwell Automation/Allen-Bradley'
     DeviceID: int = 14
-    DeviceType=None
+    DeviceType: str ="Programmable Logic Controller"
     ProductCode: int = 167
     Revision: float = 20.00
     Status: int = 12384
@@ -125,6 +198,75 @@ class PLCDeviceDTO:
     ProductNameLength: int = 11
     ProductName: str = "1756-L84E/B"
     State: int = 66
+
+    def __repr__(self):
+        props = ''
+        props += 'Length={}, '.format(self.Length)
+        props += 'EncapsulationVersion={}, '.format(self.EncapsulationVersion)
+        props += 'IPAddress={}, '.format(self.IPAddress)
+        props += 'VendorID={}, '.format(self.VendorID)
+        props += 'Vendor={}, '.format(self.Vendor)
+        props += 'DeviceID={}, '.format(self.DeviceID)
+        props += 'DeviceType={}, '.format(self.DeviceType)
+        props += 'ProductCode={}, '.format(self.ProductCode)
+        props += 'Revision={}, '.format(self.Revision)
+        props += 'Status={}, '.format(self.Status)
+        props += 'SerialNumber={}, '.format(self.SerialNumber)
+        props += 'ProductNameLength={}, '.format(self.ProductNameLength)
+        props += 'ProductName={}, '.format(self.ProductName)
+        props += 'State={}'.format(self.State)
+        return 'LGXDevice({})'.format(props)
+
+    def __str__(self):
+        ret = "{} {} {} {} {} {} {} {} {} {} {} {} {} {}".format(
+                 self.Length,
+                 self.EncapsulationVersion,
+                 self.IPAddress,
+                 self.VendorID,
+                 self.Vendor,
+                 self.DeviceID,
+                 self.DeviceType or "",
+                 self.ProductCode,
+                 self.Revision,
+                 self.Status,
+                 self.SerialNumber,
+                 self.ProductNameLength,
+                 self.ProductName,
+                 self.State)
+        return ret
+
+# Pylogix definition of a device, used for mocking
+class LGXDevice:
+
+    def __init__(self,
+                 IPAddress: str,
+                 Length: int = 0,
+                 EncapsulationVersion: int = 0,
+                 VendorID: int = 1,
+                 Vendor: str = 'Rockwell Automation/Allen-Bradley',
+                 DeviceID: int = 14,
+                 DeviceType: str | None = None,
+                 ProductCode: int = 167,
+                 Revision: float = 20.00,
+                 Status: int = 12384,
+                 SerialNumber: int = 0xffffff,
+                 ProductNameLength: int = 11,
+                 ProductName: str = "1756-L84E/B",
+                 State: int = 66):
+            self.IPAddress = IPAddress
+            self.Length =  Length
+            self.EncapsulationVersion = EncapsulationVersion
+            self.VendorID = VendorID
+            self.Vendor = Vendor
+            self.DeviceID = DeviceID
+            self.DeviceType = DeviceType
+            self.ProductCode = ProductCode
+            self.Revision = Revision
+            self.Status = Status
+            self.SerialNumber = SerialNumber
+            self.ProductNameLength = ProductNameLength
+            self.ProductName = ProductName
+            self.State = State
 
     def __repr__(self):
         props = ''
@@ -162,36 +304,44 @@ class PLCDeviceDTO:
                  self.State)
         return ret
 
-@dataclass(kw_only=True)
-class PLCResponseDTO(StatusDTO):
-    TagName: str | None
-    Value: bool | int | str | list[str] | float | datetime | list[PLCTagDTO] | PLCDeviceDTO | list[PLCDeviceDTO] | None
-    Status: str | None
+# Pylogix definition of a response, used for mocking
+class Response:
+    def __init__(self,
+                    TagName: str | None,
+                    Value: bool | int | str | list[str] | float | datetime | list[Tag] | LGXDevice | list[LGXDevice] | None,
+                    Status: str | None):
+        self.TagName = TagName
+        self.Value = Value
+        self.Status = Status
 
     def __repr__(self):
+
         return 'Response(TagName={}, Value={}, Status={})'.format(
-            self.TagName or "", self.Value or "", self.Status or "")
+            self.TagName, self.Value, self.Status)
 
     def __str__(self):
-        return '{} {} {}'.format(self.TagName or "", self.Value or "", self.Status or "")
 
+        return '{} {} {}'.format(self.TagName, self.Value, self.Status)
+
+# The apps definition of a pylogix response
 @dataclass(kw_only=True)
-class ResponseDTO(StatusDTO):
+class PLCResponseDTO:
     TagName: str | None
-    Value: bool | int | str | list[str] | float | datetime | list[PLCTagDTO] | PLCDeviceDTO | list[PLCDeviceDTO] | None
+    Value: bool | int | str | list[str] | float | datetime | list[Tag] | LGXDevice | list[LGXDevice] | None
     Status: str | None
-    error: bool
 
     def __repr__(self):
-        return 'ResponseDTO(tag={}, value={}, status={}, error={})'.format(
-            self.TagName, self.Value, self.Status, self.error)
+
+        return 'Response(TagName={}, Value={}, Status={})'.format(
+            self.TagName, self.Value, self.Status)
 
     def __str__(self):
-        return '{} {} {} {}'.format(self.TagName or "", self.Value or "", self.Status or "", self.error)
+
+        return '{} {} {}'.format(self.TagName, self.Value, self.Status)
 
 @dataclass(kw_only=True)
 class ReadResDTO(StatusDTO):
-    response: list[ResponseDTO]
+    response: list[PLCDeviceDTO]
 
 @dataclass(kw_only=True)
 class WriteReqDTO(RequestDTO):
@@ -201,7 +351,7 @@ class WriteReqDTO(RequestDTO):
 
 @dataclass(kw_only=True)
 class WriteResDTO(StatusDTO):
-    response: list[ResponseDTO]
+    response: list[PLCResponseDTO]
 
 @dataclass(kw_only=True)
 class GetPlcTimeReqDTO(RequestDTO):

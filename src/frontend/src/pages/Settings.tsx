04a16831e44a8fc16693d7eac97d744e9f47e155
William-Brumble <williambrumble@protonmail.com>
2023-07-07 20:32:39 -0500

    Removed title from settings page

--- src/frontend/src/pages/Settings.tsx
+++ src/frontend/src/pages/Settings.tsx
@@ -40,13 +40,6 @@ export function Settings() {
 
     return (
         <div className="bg-background p-5">
-            <h2 className="text-foreground mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-                Settings
-            </h2>
-            <p className="text-foreground leading-7 [&:not(:first-child)]:mt-6 pb-4">
-                Manage application settings.
-            </p>
-
             <Form {...form}>
                 <form
                     onSubmit={form.handleSubmit(onSubmit)}

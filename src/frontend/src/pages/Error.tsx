4af5fa9306bff3555047da30408b57f24a24d560
William-Brumble <williambrumble@protonmail.com>
2023-07-07 20:32:16 -0500

    Updated error page to take in params

--- src/frontend/src/pages/Error.tsx
+++ src/frontend/src/pages/Error.tsx
@@ -1,9 +1,27 @@
+import { useLoaderData } from "react-router-dom";
+
+import { ErrorAlert } from "@/components/ErrorAlert.tsx";
+
 export function Error() {
+    const { title, message }: any = useLoaderData();
+
     return (
-        <div className="bg-background p-5">
-            <h2 className="text-foreground mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-                Something went very wrong...
-            </h2>
+        <div className="bg-background p-0 m-0 p-5">
+            <ErrorAlert title={title} description={message} />
         </div>
     );
 }
+export const loader = async (payload: any) => {
+    try {
+        const params = new URL(payload.request.url).searchParams;
+        const title = params.get("title");
+        const message = params.get("message");
+
+        return { title: title, message: message };
+    } catch (error) {
+        return {
+            title: "Error",
+            response: error,
+        };
+    }
+};

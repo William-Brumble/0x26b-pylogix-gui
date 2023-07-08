3472e9ccf38cb1d99ff56335a0217d922ad49220
William-Brumble <williambrumble@protonmail.com>
2023-07-07 20:32:32 -0500

    Removed title from manual page

--- src/frontend/src/pages/ManualOperation.tsx
+++ src/frontend/src/pages/ManualOperation.tsx
@@ -24,9 +24,6 @@ export function ManualOperation() {
 
     return (
         <div className="bg-background p-5">
-            <h2 className="text-foreground mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-                Manual Operation
-            </h2>
             <p className="text-foreground leading-7 [&:not(:first-child)]:mt-6 pb-4">
                 This particular section is designated exclusively for manual
                 operation, primarily intended to facilitate the testing of

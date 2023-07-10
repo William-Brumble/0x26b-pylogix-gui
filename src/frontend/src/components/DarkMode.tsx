52e7f6c9264147a26ce7bab3f49270013594f6b7
William-Brumble <williambrumble@protonmail.com>
2023-07-10 06:28:13 -0500

    Updated dark mode to set at body element due to dialog portals being spawned from root

--- src/frontend/src/components/DarkMode.tsx
+++ src/frontend/src/components/DarkMode.tsx
@@ -1,6 +1,5 @@
 import { ReactNode, useContext } from "react";
 import { SettingsContext } from "@/store/settings.context.tsx";
-import { cn } from "@/components/lib/utils.ts";
 
 type DarkModeProps = {
     children: ReactNode;
@@ -11,14 +10,11 @@ export function DarkMode({ children }: DarkModeProps) {
      * and sets the dark tailwind css variable if enabled */
     const settings = useContext(SettingsContext);
 
-    return (
-        <div
-            className={cn(
-                settings.darkMode ? "dark" : null,
-                "bg-background p-5 w-full h-max min-h-full"
-            )}
-        >
-            {children}
-        </div>
-    );
+    if (settings.darkMode) {
+        document.body.classList.add("dark");
+    } else {
+        document.body.classList.remove("dark");
+    }
+
+    return <>{children}</>;
 }

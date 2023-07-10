c0bfa6f122e713bd800924aba6a0fd24a856759c
William-Brumble <williambrumble@protonmail.com>
2023-07-10 06:30:55 -0500

    Updated source page

--- src/frontend/src/pages/Source.tsx
+++ src/frontend/src/pages/Source.tsx
@@ -1,30 +1,53 @@
-import { discover } from "@/api";
-import { IDiscoverReq, IDiscoverRes } from "@/models/discover.ts";
+import { Suspense, useEffect } from "react";
 import {
     LoaderFunction,
     LoaderFunctionArgs,
     redirect,
     useLoaderData,
+    defer,
+    Await,
+    useNavigate,
 } from "react-router-dom";
 
+import { discover } from "@/api";
+import { IDiscoverReq, IDiscoverRes } from "@/models/discover.ts";
 import { columns } from "@/components/SourcesColumns.tsx";
 import { SourceDataTable } from "@/components/SourcesDataTable.tsx";
+import { Loading } from "@/components/Loading.tsx";
 
 export function Source() {
     const { discover_response }: any = useLoaderData();
     const encoded = discover_response as IDiscoverRes;
 
     return (
-        <div className="bg-background p-0 m-0">
-            <div className="w-full h-full min-h-full py-2">
-                <SourceDataTable
-                    columns={columns}
-                    data={encoded.response.Value}
-                />
-            </div>
-        </div>
+        <Suspense
+            fallback={
+                <Loading text="Scanning the network for available sources..." />
+            }
+        >
+            <Await resolve={encoded}>{(data) => <Table data={data} />}</Await>
+        </Suspense>
     );
 }
+
+type TableProps = {
+    data: IDiscoverRes;
+};
+const Table = ({ data }: TableProps) => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (data.error) {
+            navigate(
+                `/error?title=${data.status}&message=${data.error_message}`
+            );
+        }
+    }, []);
+
+    if (!data.error) {
+        return <SourceDataTable columns={columns} data={data.response.Value} />;
+    }
+};
 
 export const loader: LoaderFunction = async ({
     request,
@@ -37,17 +60,11 @@ export const loader: LoaderFunction = async ({
             token: token ? token : "",
         };
 
-        const sources = await discover(msg);
+        const sources = discover(msg);
 
-        if (sources.error) {
-            return redirect(
-                `/error?title=${sources.status}&message=${sources.error_message}`
-            );
-        }
-
-        return {
+        return defer({
             discover_response: sources,
-        };
+        });
     } catch (error) {
         return redirect(`/error?title=${"418 I'm a teapot"}&message=${error}`);
     }

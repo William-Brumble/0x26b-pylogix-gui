456119d464a516000fb6e61c5c8e79cdaf7d3d32
William-Brumble <williambrumble@protonmail.com>
2023-07-10 06:30:59 -0500

    Updated tags page

--- src/frontend/src/pages/Tags.tsx
+++ src/frontend/src/pages/Tags.tsx
@@ -1,21 +1,53 @@
+import { Suspense, useEffect } from "react";
 import {
+    defer,
     LoaderFunction,
     LoaderFunctionArgs,
     redirect,
     useLoaderData,
+    useNavigate,
+    Await,
 } from "react-router-dom";
 
 import { IGetTagListReq, IGetTagListRes } from "@/models/get_tag_list.ts";
 import { getTagList } from "@/api";
 import { TagDataTable } from "@/components/TagsDataTable.tsx";
 import { columns } from "@/components/TagsColumns.tsx";
+import { Loading } from "@/components/Loading.tsx";
 
 export function Tags() {
     const { tags_response }: any = useLoaderData();
     const encoded = tags_response as IGetTagListRes;
 
-    return <TagDataTable columns={columns} data={encoded.response.Value} />;
+    return (
+        <Suspense
+            fallback={
+                <Loading text="Fetching all of the tags from the source PLC..." />
+            }
+        >
+            <Await resolve={encoded}>{(data) => <Table data={data} />}</Await>
+        </Suspense>
+    );
 }
+
+type TableProps = {
+    data: IGetTagListRes;
+};
+const Table = ({ data }: TableProps) => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (data.error) {
+            navigate(
+                `/error?title=${data.status}&message=${data.error_message}`
+            );
+        }
+    }, []);
+
+    if (!data.error) {
+        return <TagDataTable columns={columns} data={data.response.Value} />;
+    }
+};
 
 export const loader: LoaderFunction = async ({
     request,
@@ -29,17 +61,11 @@ export const loader: LoaderFunction = async ({
             all_tags: true,
         };
 
-        const tags = await getTagList(reqPayload);
+        const tags = getTagList(reqPayload);
 
-        if (tags.error) {
-            return redirect(
-                `/error?title=${tags.status}&message=${tags.error_message}`
-            );
-        }
-
-        return {
+        return defer({
             tags_response: tags,
-        };
+        });
     } catch (error) {
         return redirect(`/error?title=${"418 I'm a teapot"}&message=${error}`);
     }
